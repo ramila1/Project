9e416f09548a9d12078ebc47a56b12b7f007e4f2
ramila1 <ramilashahi2799@gmail.com>
2024-05-18 11:16:18 +0545

    edit model

--- user_model_assignment/user_model_assignment/BlogPost/models.py
+++ user_model_assignment/user_model_assignment/BlogPost/models.py
@@ -1,26 +1,31 @@
 from django.db import models
 from model_app.models import CustomUser
+from django.utils import timezone
 
 class Blog(models.Model):
     title=models.CharField(max_length=50)
     description=models.TextField()
     published_date=models.DateTimeField(auto_now_add=True)
-    is_deleted=models.BooleanField(default=False)
     author=models.ForeignKey(CustomUser,null=True,on_delete=models.CASCADE,related_name='blog_post')
-
+    is_deleted = models.BooleanField(default=False)
+    deleted_at = models.DateTimeField(blank=True, null=True)
 
     def str(self):
         return self.title
 
-    def delete(self,hard_delete=False):
-        if hard_delete:
-            super().delete()
-        else:
-            self.is_deleted=True
-            self.save()
-    def restore(self):
-        self.is_deleted=False
+    def soft_delete(self):
+        self.is_deleted = True
+        self.deleted_at = timezone.now()
         self.save()
+
+    def restore(self):
+        self.is_deleted = False
+        self.deleted_at = None
+        self.save()
+    
+    def hard_delete(self):
+        self.delete()
+
 
 
 
